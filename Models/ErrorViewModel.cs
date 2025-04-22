925eee07c3f0643d16cf6ac7cf80fcc0a81d02a6
fganuarve <fganuarve@gmail.com>
2025-04-22 17:22:57 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestorViajes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

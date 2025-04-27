e17ceaa1356cee775f957122155c85f90afae5de
fganuarve <fganuarve@gmail.com>
2025-04-27 13:08:09 +0200

    Updated relations DBContext

--- Models/EFCore/Rove/RoveDbContext.cs
+++ Models/EFCore/Rove/RoveDbContext.cs
@@ -11,6 +11,11 @@ namespace GestorViajes.Models.EFCore.Rove
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<UserTrip> UserTrips { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<TripRequest> TripRequests { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -46,6 +51,109 @@ namespace GestorViajes.Models.EFCore.Rove
                       .HasForeignKey(t => t.DriverId)
                       .OnDelete(DeleteBehavior.Cascade);
 
+            });
+
+            modelBuilder.Entity<Trip>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Destination).IsRequired();
+                entity.Property(e => e.Origin).IsRequired();
+                entity.Property(e => e.Seats).IsRequired();
+                entity.Property(e => e.Active).IsRequired();
+
+                entity.Property(e => e.CreatedBy);
+                entity.Property(e => e.CreatedAt);
+                entity.Property(e => e.ModifiedAt);
+                entity.Property(e => e.ModifiedBy);
+
+
+                entity.HasOne(t => t.Driver)
+                      .WithMany(u => u.Trips)
+                      .HasForeignKey(t => t.DriverId)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+
+                entity.HasOne(t => t.Vehicle)
+                      .WithMany(v => v.Trips)
+                      .HasForeignKey(t => t.VehicleId)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+
+                entity.HasMany(t => t.TripRequests)
+                      .WithOne(tr => tr.Trip)
+                      .HasForeignKey(tr => tr.TripId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+
+                entity.HasMany(t => t.Passengers)
+                      .WithOne(ut => ut.Trip)
+                      .HasForeignKey(ut => ut.TripId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<TripRequest>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.CreatedBy);
+                entity.Property(e => e.CreatedAt);
+                entity.Property(e => e.ModifiedAt);
+                entity.Property(e => e.ModifiedBy);
+
+
+                entity.HasOne(tr => tr.User)
+                      .WithMany(u => u.TripRequests)
+                      .HasForeignKey(tr => tr.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(tr => tr.Trip)
+                      .WithMany(t => t.TripRequests)
+                      .HasForeignKey(tr => tr.TripId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<UserTrip>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+
+                entity.HasOne(ut => ut.User)
+                      .WithMany(u => u.UserTrips)
+                      .HasForeignKey(ut => ut.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+
+                entity.HasOne(ut => ut.Trip)
+                      .WithMany(t => t.Passengers)
+                      .HasForeignKey(ut => ut.TripId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<Vehicle>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Plate).IsRequired();
+                entity.Property(e => e.MaxSeats).IsRequired();
+                entity.Property(e => e.Active).IsRequired();
+
+                entity.Property(e => e.Model);
+                entity.Property(e => e.CreatedBy);
+                entity.Property(e => e.CreatedAt);
+                entity.Property(e => e.ModifiedAt);
+                entity.Property(e => e.ModifiedBy);
+
+
+                entity.HasOne(v => v.Owner)
+                      .WithMany(u => u.Vehicles)
+                      .HasForeignKey(v => v.UserId)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasMany(v => v.Trips)
+                      .WithOne(t => t.Vehicle)
+                      .HasForeignKey(t => t.VehicleId)
+                      .OnDelete(DeleteBehavior.Restrict);
             });
 
         }

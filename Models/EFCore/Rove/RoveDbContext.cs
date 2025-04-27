d92f8773da623f0be8b45c0b56c0c64ec0b66275
fganuarve <fganuarve@gmail.com>
2025-04-27 13:01:07 +0200

    navigation properties

---
+++ Models/EFCore/Rove/RoveDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorViajes.Models.EFCore.Rove
+{
+    public class RoveDbContext : DbContext
+    {
+        public RoveDbContext(DbContextOptions<RoveDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.NationalId).IsRequired();
+                entity.Property(e => e.LastName1).IsRequired();
+                entity.Property(e => e.Password).IsRequired();
+                entity.Property(e => e.Email).IsRequired();
+                entity.Property(e => e.Role).IsRequired();
+
+                entity.HasMany(u => u.TripRequests)
+                     .WithOne(tr => tr.User)
+                     .HasForeignKey(tr => tr.UserId)
+                     .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(u => u.UserTrips)
+                      .WithOne(ut => ut.User)
+                      .HasForeignKey(ut => ut.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(u => u.Vehicles)
+                      .WithOne(v => v.Owner)
+                      .HasForeignKey(v => v.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(u => u.Trips)
+                      .WithOne(t => t.Driver)
+                      .HasForeignKey(t => t.DriverId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+            });
+
+        }
+    }
+}

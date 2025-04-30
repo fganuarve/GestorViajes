8e3d7c78bc8f14e554a2795e7c104935ed7acf01
fganuarve <fganuarve@gmail.com>
2025-04-30 15:08:33 +0200

    Migrations for adding colors to vehicles

---
+++ Migrations/20250430130730_Update_Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestorViajes.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Vehicle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Vehicles");
+        }
+    }
+}
